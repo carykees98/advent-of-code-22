ac0855dacdaf18b070635d3e39ca4e1c05edae37
MasterManiac980 <trainboy980@gmail.com>
2022-12-02 23:56:31 -0500

    added day 3

---
+++ day3/day3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+	std::ifstream infile("day3.txt");
+	int answer;
+
+	while (infile >>)
+	{
+	}
+
+	std::cout << answer << std::endl;
+	return 0;
+}
