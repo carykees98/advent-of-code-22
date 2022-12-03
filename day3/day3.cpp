835aee1ea140e9e43958cffba1befd5540d2c425
MasterManiac980 <trainboy980@gmail.com>
2022-12-03 01:09:43 -0500

    finished day 3

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -2,16 +2,64 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 int main(int argc, char const *argv[])
 {
 	std::ifstream infile("day3.txt");
-	int answer;
+	int answer1 = 0;
+	int answer2 = 0;
+	std::string line;
+	std::string firstHalf, secondHalf, alreadyUsed;
 
-	while (infile >>)
+	while (infile >> line)
 	{
+		firstHalf = line.substr(0, line.length() / 2);
+		secondHalf = line.substr(line.length() / 2);
+
+		for (char &character : firstHalf)
+		{
+			if ((secondHalf.find(character) != std::string::npos) && (alreadyUsed.find(character) == std::string::npos))
+			{
+				if (isupper(character))
+				{
+					answer1 += character - 38;
+				}
+				else if (islower(character))
+				{
+					answer1 += character - 96;
+				}
+				alreadyUsed.push_back(character);
+			}
+		}
+		alreadyUsed.clear();
 	}
 
-	std::cout << answer << std::endl;
+	std::string line2, line3;
+	infile.close();
+	infile.open("day3.txt");
+
+	while (infile >> line >> line2 >> line3)
+	{
+		for (char &character : line)
+		{
+			if ((line2.find(character) != std::string::npos) && (line3.find(character) != std::string::npos) && (alreadyUsed.find(character) == std::string::npos))
+			{
+				if (isupper(character))
+				{
+					answer2 += character - 38;
+				}
+				else if (islower(character))
+				{
+					answer2 += character - 96;
+				}
+				alreadyUsed.push_back(character);
+			}
+		}
+		alreadyUsed.clear();
+	}
+
+	std::cout << "Answer 1: " << answer1 << std::endl;
+	std::cout << "Answer 2: " << answer2 << std::endl;
 	return 0;
 }
