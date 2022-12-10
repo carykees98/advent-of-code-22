1d8f1cf73ae2e3fcf6daeff0f4e12054d0a1c1fe
MasterManiac980 <trainboy980@gmail.com>
2022-12-09 23:18:34 -0500

    added day 10

---
+++ day10/day10.cpp
@@ -0,0 +1,24 @@
+#include "../aoc-utils/aocIncludes.h"
+
+int main(int argc, char const *argv[])
+{
+	// Open File
+	std::string fileName(__FILE_NAME__);
+	fileName.replace(fileName.end() - 3, fileName.end(), "txt");
+	std::ifstream infile(fileName);
+
+	// Variable Declarations
+	int answer1 = 0, answer2 = 0;
+	std::string line;
+
+	// Solution
+	while (infile >> line)
+	{
+	}
+
+	// Print Answer
+	std::cout << "Answer 1: " << answer1 << std::endl
+			  << "Answer 2: " << answer2 << std::endl;
+
+	return 0;
+}
